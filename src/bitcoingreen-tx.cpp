4d53a4cd1e00557f90b239441179ed82a1c87e69
AtomicLemon <admin@flame.exchange>
2019-03-31 09:18:46 +1300

    More rename

--- src/bitcoingreen-tx.cpp
+++ src/bitcoingreen-tx.cpp
@@ -50,8 +50,8 @@ static bool AppInitRawTx(int argc, char* argv[])
         // First part of help message is specific to this utility
         std::string strUsage = _("Units Core bitcoingreen-tx utility version") + " " + FormatFullVersion() + "\n\n" +
                                _("Usage:") + "\n" +
-                               "  bitcoingreen-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded bitcoin green transaction") + "\n" +
-                               "  bitcoingreen-tx [options] -create [commands]   " + _("Create hex-encoded bitcoin green transaction") + "\n" +
+                               "  bitcoingreen-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded units transaction") + "\n" +
+                               "  bitcoingreen-tx [options] -create [commands]   " + _("Create hex-encoded units transaction") + "\n" +
                                "\n";
 
         fprintf(stdout, "%s", strUsage.c_str());
@@ -553,7 +553,7 @@ static int CommandLineRawTx(int argc, char* argv[])
             if (argc < 2)
                 throw runtime_error("too few parameters");
 
-            // param: hex-encoded bitcoin green transaction
+            // param: hex-encoded units transaction
             string strHexTx(argv[1]);
             if (strHexTx == "-") // "-" implies standard input
                 strHexTx = readStdin();
--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -239,7 +239,7 @@ UniValue getaddednodeinfo(const UniValue& params, bool fHelp)
             "    \"connected\" : true|false,          (boolean) If connected\n"
             "    \"addresses\" : [\n"
             "       {\n"
-            "         \"address\" : \"192.168.0.201:9333\",  (string) The bitcoin green server host and port\n"
+            "         \"address\" : \"192.168.0.201:9333\",  (string) The units server host and port\n"
             "         \"connected\" : \"outbound\"           (string) connection, inbound or outbound\n"
             "       }\n"
             "       ,...\n"
